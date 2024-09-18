1d0b15f2629fdb0bedee3aaa9dbf3c74ed77ded5
Marcia <hmarcia40@gmail.com>
2024-09-18 00:07:12 -0400

    pascals triangle

--- tier-3/pascals-triangle.js
+++ tier-3/pascals-triangle.js
@@ -11,13 +11,16 @@ module.exports = function(row){
     
     for (let i = 1; i < row; i++) {
         const prevRow = triangle[i - 1];
+        //every row in Pascal's Triangle starts and ends with 1
         const newRow = [1];
-        
+        // start at index 1 (because index 0 is always 1)
         for (let j = 1; j < i; j++) {
+          // sum up the two adjacent values from the previous row
             newRow.push(prevRow[j - 1] + prevRow[j]);
         }
-        
+        //every row ends with 1
         newRow.push(1);
+        //add the newRow to the triangle array
         triangle.push(newRow);
     }
     
