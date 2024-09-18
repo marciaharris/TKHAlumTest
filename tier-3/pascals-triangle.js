4beac52645ef84275c62a1fc426fffd143835678
Marcia <hmarcia40@gmail.com>
2024-09-17 23:55:48 -0400

    pascals triangle

--- tier-3/pascals-triangle.js
+++ tier-3/pascals-triangle.js
@@ -5,4 +5,21 @@
 
 module.exports = function(row){
   //Solution goes here
+  if (row === 0) return [];
+    
+    const triangle = [[1]];
+    
+    for (let i = 1; i < row; i++) {
+        const prevRow = triangle[i - 1];
+        const newRow = [1];
+        
+        for (let j = 1; j < i; j++) {
+            newRow.push(prevRow[j - 1] + prevRow[j]);
+        }
+        
+        newRow.push(1);
+        triangle.push(newRow);
+    }
+    
+    return triangle;
 }
